511c13674fcecd692fd98af6820fe8e70b618c09
pratik bhandari <pratikbhandari9292@gmail.com>
2020-12-27 16:32:44 +0545

    updated the app.js file to use hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
@@ -31,13 +31,9 @@ import FavoritesPage from "./pages/favorites/favorites";
 //this is the top level component of the application
 //this is where other components are brought to be rendered
 //this is where we also define routes to various parts of the application
-class App extends React.Component {
-	//this is a placeholder for the function we get back from auth.onAuthStateChanged()
-	//it unsubscribes / closes all connection with the auth library
-	unSubscribeFromAuth = null;
-
-	setCurrentTheme = () => {
-		const { setTheme } = this.props;
+const App = (props) => {
+	const setCurrentTheme = () => {
+		const { setTheme } = props;
 
 		const localStorage = window.localStorage;
 		const localStorageCurrentTheme = localStorage.getItem("currentTheme");
@@ -46,34 +42,42 @@ class App extends React.Component {
 		}
 	};
 
-	componentDidMount() {
-		const { updateCurrentUser, toggleNotification } = this.props;
+	useEffect(() => {
+		const { updateCurrentUser, toggleNotification } = props;
 
-		this.setCurrentTheme();
+		setCurrentTheme();
 
 		//whenever the authentication state in our application changes
-		this.unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-			//this userAuth is either an object representing the user that is currently sign in
-			//or null meaning that no one is signed in or a user has signed out
-			if (userAuth) {
-				const userRef = await createUserDocument(userAuth);
+		const unSubscribeFromAuth = auth.onAuthStateChanged(
+			async (userAuth) => {
+				//this userAuth is either an object representing the user that is currently sign in
+				//or null meaning that no one is signed in or a user has signed out
+				if (userAuth) {
+					const userRef = await createUserDocument(userAuth);
 
-				userRef.onSnapshot((snapShot) => {
-					updateCurrentUser({ ...snapShot.data() });
-					toggleNotification("signed in successfully", "success");
-				});
-			} else {
-				updateCurrentUser(userAuth);
+					userRef.onSnapshot((snapShot) => {
+						updateCurrentUser({ ...snapShot.data() });
+						toggleNotification("signed in successfully", "success");
+					});
+				} else {
+					updateCurrentUser(userAuth);
+				}
 			}
-		});
-	}
+		);
 
-	componentDidUpdate(prevProps) {
-		const { currentUser, fetchFavoritesSuccess } = this.props;
+		return () => {
+			//whenever the application closes, we unsubscribe from the auth
+			//library, preventing any memory leaks
+			unSubscribeFromAuth();
+		};
+	}, []);
+
+	useEffect(() => {
+		const { currentUser, fetchFavoritesSuccess } = props;
 
 		//if previously available current user is different from the currently available current user
 		//and also if it actually exists (not null)
-		if (prevProps.currentUser !== currentUser && currentUser) {
+		if (currentUser) {
 			//here we are fetcing the favorites of the current user as soon as they sign in
 			getFavoritesCollectionRef(currentUser.id).then((collectionRef) => {
 				collectionRef.onSnapshot(async (snapShot) => {
@@ -85,93 +89,85 @@ class App extends React.Component {
 				});
 			});
 		}
-	}
+	}, [props.currentUser]);
 
-	componentWillUnmount() {
-		//whenever the application closes, we unsubscribe from the auth
-		//library, preventing any memory leaks
-		this.unSubscribeFromAuth();
-	}
+	const {
+		currentTheme,
+		currentUser,
+		notificationMessage,
+		notificationType,
+	} = props;
 
-	render() {
-		const {
-			currentTheme,
-			currentUser,
-			notificationMessage,
-			notificationType,
-		} = this.props;
-
-		return (
-			<BrowserRouter>
-				<ThemeProvider
-					theme={currentTheme === "dark" ? darkTheme : lightTheme}
-				>
-					<div className="app">
-						<GlobalStyles />
-						<Header />
-						<Notification
-							message={notificationMessage}
-							type={notificationType}
+	return (
+		<BrowserRouter>
+			<ThemeProvider
+				theme={currentTheme === "dark" ? darkTheme : lightTheme}
+			>
+				<div className="app">
+					<GlobalStyles />
+					<Header />
+					<Notification
+						message={notificationMessage}
+						type={notificationType}
+					/>
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={() => <Redirect to="/movies" />}
 						/>
-						<Switch>
-							<Route
-								exact
-								path="/"
-								render={() => <Redirect to="/movies" />}
-							/>
-							<Route path="/movies" component={MoviesPage} />
-							<Route path="/tvshows" component={TvShowsPage} />
-							<Route
-								path="/search/:query"
-								component={SearchResultsPage}
-							/>
-							<Route
-								path="/details/:type/:id"
-								component={DetailsPage}
-							/>
-							<Route
-								path="/signin"
-								render={() => {
-									//if the user signs in
-									//they are redirected to the home
-									//same if they are currently signed in and
-									//navigated to the signin page
-									return currentUser ? (
-										<Redirect to="/movies" />
-									) : (
-										<SignInPage />
-									);
-								}}
-							/>
-							<Route
-								path="/signup"
-								render={() => {
-									//same logic as with the signin page
-									return currentUser ? (
-										<Redirect to="/movies" />
-									) : (
-										<SignUpPage />
-									);
-								}}
-							/>
-							<Route
-								path="/favorites"
-								render={() => {
-									//same logic as with the signin page
-									return currentUser ? (
-										<FavoritesPage />
-									) : (
-										<Redirect to="/signin" />
-									);
-								}}
-							/>
-						</Switch>
-					</div>
-				</ThemeProvider>
-			</BrowserRouter>
-		);
-	}
-}
+						<Route path="/movies" component={MoviesPage} />
+						<Route path="/tvshows" component={TvShowsPage} />
+						<Route
+							path="/search/:query"
+							component={SearchResultsPage}
+						/>
+						<Route
+							path="/details/:type/:id"
+							component={DetailsPage}
+						/>
+						<Route
+							path="/signin"
+							render={() => {
+								//if the user signs in
+								//they are redirected to the home
+								//same if they are currently signed in and
+								//navigated to the signin page
+								return currentUser ? (
+									<Redirect to="/movies" />
+								) : (
+									<SignInPage />
+								);
+							}}
+						/>
+						<Route
+							path="/signup"
+							render={() => {
+								//same logic as with the signin page
+								return currentUser ? (
+									<Redirect to="/movies" />
+								) : (
+									<SignUpPage />
+								);
+							}}
+						/>
+						<Route
+							path="/favorites"
+							render={() => {
+								//same logic as with the signin page
+								return currentUser ? (
+									<FavoritesPage />
+								) : (
+									<Redirect to="/signin" />
+								);
+							}}
+						/>
+					</Switch>
+				</div>
+			</ThemeProvider>
+		</BrowserRouter>
+	);
+};
 
 const mapStateToProps = (state) => {
 	return {
